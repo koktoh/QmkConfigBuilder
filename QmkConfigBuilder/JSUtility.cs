dc091b0b6adb2057fb521f176f61e341ab24fdf8
koktoh <koktoh96@gmail.com>
2023-03-08 17:44:36 +0900

    add Project

---
+++ QmkConfigBuilder/JSUtility.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.JSInterop;
+
+namespace QmkConfigBuilder
+{
+    public class JSUtility : IDisposable
+    {
+        private readonly IJSRuntime _js;
+
+        public JSUtility(IJSRuntime js)
+        {
+            this._js = js;
+        }
+
+        private async Task<IJSObjectReference> GetJSModuleAsync()
+        {
+            return await this._js.InvokeAsync<IJSObjectReference>("import", "./js/utility.js");
+        }
+
+        public async ValueTask<double> GetElementHeightAsync(string tag)
+        {
+            var exists = await this.ExistsElementAsync(tag);
+
+            if (!exists)
+            {
+                return 0;
+            }
+
+            var module = await this.GetJSModuleAsync();
+
+            return await module.InvokeAsync<double>("getElementHeight", tag);
+        }
+
+        public async ValueTask<double> GetElementWidthAsync(string tag)
+        {
+            var exists = await this.ExistsElementAsync(tag);
+
+            if (!exists)
+            {
+                return 0;
+            }
+
+            var module = await this.GetJSModuleAsync();
+
+            var width = await module.InvokeAsync<double>("getElementWidth", tag);
+
+            return width;
+        }
+
+        public async ValueTask<bool> ExistsElementAsync(string tag)
+        {
+            var module = await this.GetJSModuleAsync();
+
+            return await module.InvokeAsync<bool>("exists", tag);
+        }
+
+        public async ValueTask<dynamic> GetElementById(string id)
+        {
+            var module = await this.GetJSModuleAsync();
+
+            return await module.InvokeAsync<dynamic>("getElement", id);
+        }
+
+        public async ValueTask<IEnumerable<dynamic>> GetElementsById(IEnumerable<string> ids)
+        {
+            var module = await this.GetJSModuleAsync();
+
+            return await module.InvokeAsync<IEnumerable<object>>("getElementsById", ids);
+        }
+
+        public async ValueTask<string> GetCssPropertyValueAsync(string selector, string propertyName)
+        {
+            var module = await this.GetJSModuleAsync();
+
+            return await module.InvokeAsync<string>("getCssPropertyValue", selector, propertyName);
+        }
+
+        public async ValueTask ShowAsync(string selector)
+        {
+            var module=await this.GetJSModuleAsync();
+
+            await module.InvokeVoidAsync("show", selector);
+        }
+
+        public async ValueTask HideAsync(string selector)
+        {
+            var module = await this.GetJSModuleAsync();
+
+            await module.InvokeVoidAsync("hide", selector);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ QmkConfigBuilder/wwwroot/js/utility.js
@@ -0,0 +1,27 @@
+﻿export function getElementHeight(id) {
+    return $(id).height();
+}
+
+export function getElementWidth(id) {
+    return $(id).width();
+}
+
+export function exists(id) {
+    return $(id).length > 0 ? true : false;
+}
+
+export function getElement(id) {
+    return document.getElementById(id);
+}
+
+export function getCssPropertyValue(selector, propertyName) {
+    return $(selector).css(propertyName);
+}
+
+export function show(selector) {
+    $(selector).show();
+}
+
+export function hide(selector) {
+    $(selector).hide();
+}
