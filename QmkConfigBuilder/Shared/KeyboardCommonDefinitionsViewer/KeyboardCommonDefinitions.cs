d2eb79450780db4ec6a58a03de0b25d313fa8b34
koktoh <koktoh96@gmail.com>
2023-03-26 01:45:58 +0900

    add Keyboard Common Definitions View

---
+++ QmkConfigBuilder/Shared/KeyboardCommonDefinitionsViewer/KeyboardCommonDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using QmkConfigBuilder.Models.KeyboardDefinitions;
+
+namespace QmkConfigBuilder.Shared.KeyboardCommonDefinitionsViewer
+{
+    public class KeyboardCommonDefinitions
+    {
+        public string VID { get; set; } = string.Empty;
+        public string PID { get; set; } = string.Empty;
+        public string Manufacturer { get; set; } = string.Empty;
+        public string Maintainer { get; set; } = string.Empty;
+        public string KeyboardName { get; set; } = string.Empty;
+        public DiodeDirection DiodeDirection { get; set; } = DiodeDirection.COL2ROW;
+    }
+}
